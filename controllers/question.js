1c7804bb9bfe0a106db9c6db953c9c34eea9d631
Anil <kanilreddy867@gmail.com>
2023-12-20 11:37:55 +0530

    rechecked all the routes and they are working fine.

--- controllers/question.js
+++ controllers/question.js
@@ -11,7 +11,10 @@ async function newQuestion(req, res) {
       endDate,
     });
     const savedQuestion = await newQuestionInstance.save();
-    res.status(201).json(savedQuestion);
+    res.status(201).json({
+      message: "Question created successfully",
+      question: savedQuestion,
+    });
   } catch (err) {
     res
       .status(500)
@@ -22,20 +25,32 @@ async function newQuestion(req, res) {
 async function getActiveQuiz(req, res) {
   try {
     const currentDate = new Date();
+    
+    // Add 5 hours and 30 minutes to the current date to make it IST
+    currentDate.setHours(currentDate.getHours() + 5);
+    currentDate.setMinutes(currentDate.getMinutes() + 30);
+
+    // console.log(currentDate);
+
     const activeQuiz = await questionModel.findOne({
       startDate: { $lte: currentDate },
       endDate: { $gte: currentDate },
     });
 
     if (!activeQuiz) {
-      return res.status(404).json({ message: "No active quiz found" });
+      return res.status(404).json({ message: "No active quiz found at the moment" });
     }
 
-    res.json(activeQuiz);
+    res.json({
+      message: "Active quiz found",
+      activeQuiz,
+      currentTimeIST: currentDate.toISOString(),
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 }
+
 
 async function getQuizResult(req, res) {
   try {
@@ -49,26 +64,46 @@ async function getQuizResult(req, res) {
     const endDate = new Date(quiz.endDate);
     const fiveMinutesAfterEnd = new Date(endDate.getTime() + 5 * 60000);
 
-    if (new Date() < fiveMinutesAfterEnd) {
+    // console.log(endDate, fiveMinutesAfterEnd);
+    const currDate = new Date();
+
+    // Add 5 hours and 30 minutes to the current date to make it IST
+    currDate.setHours(currDate.getHours() + 5);
+    currDate.setMinutes(currDate.getMinutes() + 30);
+
+    if (currDate < fiveMinutesAfterEnd) {
       return res.status(403).json({ message: "Results not available yet" });
     }
 
     res.json({
+      message: "Quiz results available",
+      question: quiz.question,
       correctAnswer: quiz.options[quiz.rightAnswer],
-      // Add additional information if needed
+      quizId: quiz._id,
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 }
 
+
 async function getAllQuizzes(req, res) {
   try {
     const allQuizzes = await questionModel.find();
-    res.json(allQuizzes);
+
+    if (!allQuizzes || allQuizzes.length === 0) {
+      return res.status(404).json({ message: "No quizzes found", status: 404 });
+    }
+
+    res.status(200).json({
+      message: "Quizzes retrieved successfully",
+      totalQuizzes: allQuizzes.length,
+      quizzes: allQuizzes,
+    });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ message: "Internal server error", status: 500, error: error.message });
   }
 }
+
 
 module.exports = { newQuestion, getActiveQuiz, getQuizResult, getAllQuizzes };
